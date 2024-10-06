1e62b4eda1ef0f07cd5feb04b248dcbd6fce5348
Nagham <Naghamkheir25@gmail.com>
2024-10-06 05:44:54 -0400

    Connecting GitHub Pages Frontend to the Backend

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Connect to WebSocket server
-const socket = new WebSocket('ws://localhost:5500');
+const socket = new WebSocket('ws:https://nk25719.github.io/PMD-Support/');
 
 // Handle incoming WebSocket messages (sensor updates)
 socket.onmessage = (event) => {
@@ -19,7 +19,7 @@ function sendEmergency(level) {
 
 // Fetch sensor data via HTTP request
 function fetchData() {
-    fetch('http://localhost:5500/update')
+    fetch('https://nk25719.github.io/PMD-Support/update')
         .then(response => response.json())
         .then(data => {
             document.getElementById('Value').innerText = 'Sensor Value: ' + data.value;
